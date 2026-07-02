4fe884af88785c5dfb959782a54f0a9cf3d6331b
hchartie <hchartie@student.42.fr>
2026-07-02 15:24:55 +0200

    Init ex03

---
+++ ex03/HumanA.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/02 15:21:27 by hchartie          #+#    #+#             */
+/*   Updated: 2026/07/02 15:24:32 by hchartie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+# include <iostream>
+
+class HumanA
+{
+private:
+public:
+    HumanA();
+    ~HumanA();
+};
+
+#endif
---
+++ ex03/HumanB.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/02 15:23:05 by hchartie          #+#    #+#             */
+/*   Updated: 2026/07/02 15:23:59 by hchartie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+# include <iostream>
+
+class HumanB
+{
+private:
+public:
+    HumanB();
+    ~HumanB();
+};
+
+#endif
